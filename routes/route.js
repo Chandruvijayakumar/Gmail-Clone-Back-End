540a6ca4aaff8b76567852def428bd907f30dd07
chandru vijayakumar <chandru4395393@gmail.com>
2024-10-02 12:29:16 +0530

    last commit

--- routes/route.js
+++ routes/route.js
@@ -1,12 +1,18 @@
-import express from 'express';
-import { saveSentEmails,getEmails,moveEmailsToBin,toggleStarredEmails,deleteEmails} from '../controller/email-controller.js';
+import express from "express";
+import {
+  saveSentEmails,
+  getEmails,
+  moveEmailsToBin,
+  toggleStarredEmails,
+  deleteEmails,
+} from "../controllers/email-controller.js";
 
-const routes=express.Router();
-routes.post('/save',saveSentEmails);
-routes.get('/emails/:type',getEmails);
-routes.post('/save-draft',saveSentEmails);
-routes.post('/bin',moveEmailsToBin);
-routes.post('/starred',toggleStarredEmails);
+const routes = express.Router();
+routes.post("/save", saveSentEmails);
+routes.get("/emails/:type", getEmails);
+routes.post("/save-draft", saveSentEmails);
+routes.post("/bin", moveEmailsToBin);
+routes.post("/starred", toggleStarredEmails);
 
-routes.delete('/delete',deleteEmails);
+routes.delete("/delete", deleteEmails);
 export default routes;
